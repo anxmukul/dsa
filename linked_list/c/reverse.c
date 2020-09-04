e0e77a3af1b585473c9d1f89b36d7c18ea9c55a4
Mukul <kmukul1197@gmail.com>
2020-09-04 11:10:28 +0530

    Reverse of a linked list

--- linked_list/c/reverse.c
+++ linked_list/c/reverse.c
@@ -26,6 +26,20 @@ void create_linked_list(){
         }
     }
 }
+void reverse(){
+    printf("Reverse called:\n");
+    struct node *cur, *prv, *far;
+    cur = head;
+    prv = NULL;
+    while(cur != NULL){
+        //printf("Inside while:");
+        far = cur->next;
+        cur->next = prv;
+        prv = cur;
+        cur = far;
+    }
+    head = prv;
+}
 void display_all_element(struct node *head){
     struct node* p;
     printf("Data in the nodes are:-\n");
@@ -36,5 +50,7 @@ void display_all_element(struct node *head){
 int main(){
     create_linked_list();
     display_all_element(head);
-   
+    reverse();
+    printf("Complete reverse:\n");
+    display_all_element(head);
 }
