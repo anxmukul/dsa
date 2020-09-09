4e0ea9e7fc8ea54693bf34cb30f58723ba4b6dcb
Mukul <kmukul1197@gmail.com>
2020-09-09 12:49:23 +0530

    Converting a expression from infix to postfix

---
+++ stacks/infixtopostfix.c
@@ -0,0 +1,119 @@
+//Converting infix expression to postfix.
+#include <stdio.h>
+#include <stdio.h>
+#define MAX 20
+typedef struct{
+    char data[MAX];
+    int top;
+}STACK;
+
+void push(STACK *s, int v){
+    if(s->top == MAX -1){
+        printf("Stack Overflow\n");
+        //return 1;
+    }
+    s->top++;
+    s->data[s->top] = v;
+    //return 0;
+}
+void pop(STACK *s, int *v){
+    if(s->top == -1){
+        printf("Stack Underflow\n");
+        //return 1;
+    }
+    *v = s->data[s->top];
+    s->top--;
+    //return 0;
+}
+int get_v(char o){
+    int t;
+    switch(o){
+        case '+':
+        case '-':
+        t =1;
+        break;
+        case '*':
+        case '/':
+        t = 2;
+        break;
+        case '^':
+        t = 3;
+        break;
+    }
+    return t;
+}
+int is_operand(int c){
+    if(65<= c >= 90){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+int is_LtoH(char o1, char o2){
+    if(get_v(o1) < get_v(o2)){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+void infix_to_postfix(char *inp, char *out){
+    STACK s1;
+    s1.top = -1;
+    int i=0, k=0;
+    char m,n;
+    while(inp[i] != '\0'){
+    if(is_operand(inp[i])){
+        out[k++] = inp[i];
+        }
+        else if(inp[i] == '('){
+            push(&s1, inp[i]);
+        }
+        else if(inp[i] == ')'){
+            while(1){
+                pop(&s1, &m);
+                if(m == "("){
+                    break;
+                }
+                out[k++] = m;
+            }
+        }
+        else{
+            if(s1.top == -1){
+                push(&s1, inp[i]);
+            }
+            else{
+                pop(&s1,&n);
+                if(n == '('){
+                    push(&s1, n);
+                    push(&s1, inp[i]);
+                }
+                else if(is_LtoH(n, inp[i])){
+                    push(&s1, n);
+                    push(&s1, inp[i]);
+                }
+                else{
+                    out[k++] = n;
+                    i--;
+                    continue;
+                }
+            }
+        }
+        i += 1;
+    }
+    while(s1.top != -1){
+        pop(&s1, &m);
+        out[k++] = m;
+    }
+}
+int main(){
+    int i = 0;
+    char inp[] = {'a','/','b','-','k','*','(','d','-','e','*','f','+','g',')','/','p'};
+    char out[MAX];
+    infix_to_postfix(inp, out);
+    while(out[i] != '\0'){
+        printf("%c ", out[i]);
+        i++;
+    }
+}
