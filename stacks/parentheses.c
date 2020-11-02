51ec53f6792ce0b13f4bf38f2e9957427b985b9c
Mukul <kmukul1197@gmail.com>
2020-11-02 11:49:11 +0530

    Fixing error

--- stacks/parentheses.c
+++ stacks/parentheses.c
@@ -1,17 +1,23 @@
 //Program for checking wether the parentheses are balanced or not.
 #include <stdio.h>
 #include <stdlib.h>
-struct node {
+
+struct node
+{
     char par;
     struct node *next;
 };
-typedef struct{
+typedef struct
+{
     struct node *top;
-}STACK;
-int push(STACK *s, char v){
+} STACK;
+int push(STACK *s, char v)
+{
     struct node *cur;
-    cur = (struct node*)malloc(sizeof(struct node));
-    if(cur == NULL){
+    cur = (struct node *)malloc(sizeof(struct node));
+
+    if (cur == NULL)
+    {
         printf("Error: Stack Overflow\n");
         return 1;
     }
@@ -19,11 +25,14 @@ int push(STACK *s, char v){
     cur->next = NULL;
     cur->next = s->top;
     s->top = cur;
+
     return 0;
 }
-int pop(STACK *s){
+int pop(STACK *s)
+{
     struct node *ptr;
-    if(s->top == NULL){
+    if (s->top == NULL)
+    {
         printf("Unbalanced\n");
         return 1;
     }
@@ -32,39 +41,50 @@ int pop(STACK *s){
     free(ptr);
     return 0;
 }
-void check(STACK *s){
-    if(s->top != NULL){
+void check(STACK *s)
+{
+    if (s->top != NULL)
+    {
         printf("Unbalanced parantheses\n");
     }
-    else{
+    else
+    {
         printf("Balanced\n");
     }
 }
-int main(){
+int main()
+{
     STACK s1;
     s1.top = NULL;
     char p;
     int n;
     printf("Enter C if you want to check at the end\nPress E exit the program\n");
     printf("Enter the parentheses:\n");
-    while(1>0){
+    while (1 > 0)
+    {
         scanf("%c", &p);
-        if(p == '('){
+        if (p == '(')
+        {
             int k = push(&s1, p);
         }
-        else if(p == ')'){
+        else if (p == ')')
+        {
             int k = pop(&s1);
-              if(k == 1){
+            if (k == 1)
+            {
                 break;
             }
         }
-        else if(p == 'C'){
+        else if (p == 'C')
+        {
             check(&s1);
         }
-        else if(p == 'E'){
+        else if (p == 'E')
+        {
             break;
         }
-        else{
+        else
+        {
             printf("Invalid Input\n");
         }
     }
