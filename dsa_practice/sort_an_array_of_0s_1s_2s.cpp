17fb5902804bc5e6e8d20e40456a3f551b538c00
Mukul <kmukul1197@gmail.com>
2021-06-16 13:22:38 +0530

    Sorting using Dutch national flag algo

--- dsa_practice/sort_an_array_of_0s_1s_2s.cpp
+++ dsa_practice/sort_an_array_of_0s_1s_2s.cpp
@@ -1,27 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
+// void sort012(int a[], int n)
+// {
+// 	int zero = 0, one =0, two = 0, j, k;
+// 	for(int i=0; i<n; i++){
+// 		if(a[i] == 1){
+// 			one++;
+// 		}
+// 		else if(a[i] == 0){
+// 			zero++;
+// 		}
+// 		else{
+// 			two++;
+// 		}
+// 	}
+// 	for(int i=0; i<zero; i++){
+// 		a[i] = 0;
+// 	}
+// 	for(int i=zero; i<zero+one; i++){
+// 		a[i] = 1;
+// 	}
+// 	for(int i=zero+one; i<n; i++){
+// 		a[i] = 2;
+// 	}
+// }
+//Using dutch national flag algorithm
 void sort012(int a[], int n)
 {
-	int zero = 0, one =0, two = 0, j, k;
-	for(int i=0; i<n; i++){
-		if(a[i] == 1){
-			one++;
+	int low = 0, mid = 0, high = n - 1;
+	while (mid <= high)
+	{
+		if (a[mid] == 0)
+		{
+			int temp = a[mid];
+			a[mid] = a[low];
+			a[low] = temp;
+			low++;
+			mid++;
 		}
-		else if(a[i] == 0){
-			zero++;
+		else if (a[mid] == 1)
+		{
+			mid++;
 		}
-		else{
-			two++;
+		else if (a[mid] == 2)
+		{
+			int temp = a[mid];
+			a[mid] = a[high];
+			a[high] = temp;
+			high--;
 		}
-	}
-	for(int i=0; i<zero; i++){
-		a[i] = 0;
-	}
-	for(int i=zero; i<zero+one; i++){
-		a[i] = 1;
-	}
-	for(int i=zero+one; i<n; i++){
-		a[i] = 2;
 	}
 }
 int main()
