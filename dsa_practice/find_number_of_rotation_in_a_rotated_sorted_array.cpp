f0c68629ad6538eb3dd3b62107797458ad1d85a4
Mukul <kmukul1197@gmail.com>
2021-06-17 19:34:53 +0530

    Index of minimum element in rotated sorted array

---
+++ dsa_practice/find_number_of_rotation_in_a_rotated_sorted_array.cpp
@@ -0,0 +1,46 @@
+// No. of rotation in rottated sorted array will be equal to
+// INdex of minimum element in that array
+#include <bits/stdc++.h>
+using namespace std;
+int searchIndex(int a[], int n)
+{
+	int start = 0, end = n - 1, mid, next, prev, pivot = -1;
+	while (start <= end)
+	{
+		
+		int mid = start + (end - start) / 2;
+		int next = (mid + 1) % n;
+		int prev = (mid - 1 + n) % n;
+		if (a[mid] <= a[next] && a[mid] <= a[prev])
+		{
+
+			pivot = mid;
+			break;
+		}
+		if (a[start] <= a[mid])
+		{
+
+			start = mid + 1;
+		}
+		else if (a[mid] <= a[end])
+		{
+			end = mid - 1;
+		}
+	}
+	return pivot;
+}
+int main()
+{
+	int no;
+	cout << "Enter number of element\n";
+	cin >> no;
+	int arr[no];
+	cout << "Enter values of rotated sorted array\n";
+	for (int i = 0; i < no; i++)
+	{
+		cin >> arr[i];
+	}
+	int x = searchIndex(arr, no); // Is a binary seach based algorithm
+	cout << "Min value index or pivot is = " << x << endl;
+	return 0;
+}
