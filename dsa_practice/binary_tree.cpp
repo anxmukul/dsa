63fae75e595960033dc970df2adacadf9e05699f
Mukul <kmukul1197@gmail.com>
2021-06-23 12:32:26 +0530

    Binary_tree_creation

---
+++ dsa_practice/binary_tree.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct node
+{
+    node *left;
+    int data;
+    node *right;
+};
+node *create_node(int v)
+{
+    node *cur = new node();
+    if (cur != NULL)
+    {
+        cur->left = cur->right = NULL;
+        cur->data = v;
+    }
+    return cur;
+}
+void display_preorder(struct node *r)
+{
+    if (r != NULL)
+    {
+        printf("%d ", r->data);
+        display_preorder(r->left);
+        display_preorder(r->right);
+    }
+}
+int main()
+{
+    node *root = NULL;
+    root = create_node(4);
+    root->left = create_node(9);
+    root->left->right = create_node(2);
+    root->left->left = create_node(3);
+    root->left->left->right = create_node(1);
+    root->right = create_node(8);
+    root->right->left = create_node(7);
+    root->right->right = create_node(6);
+	root->right->right->left = create_node(45);
+    display_preorder(root);
+	cout<<"\n";
+}
+
