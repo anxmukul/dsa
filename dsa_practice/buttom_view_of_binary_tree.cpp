0debfd697a117f40aea165058ae07fedce52141d
Mukul <kmukul1197@gmail.com>
2021-06-26 13:41:11 +0530

    Buttom view of binay tree

---
+++ dsa_practice/buttom_view_of_binary_tree.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct node
+{
+    node *left;
+    int data;
+    node *right;
+};
+node *create_node(int v)
+{
+    node *cur = new node();
+    if (cur != NULL)
+    {
+        cur->left = cur->right = NULL;
+        cur->data = v;
+    }
+    return cur;
+}
+vector<int> buttomView(node *root){
+    vector <int> result;
+    if(root == NULL){
+        return result;
+    }
+    map <int, int> m;
+    queue <pair<node *, int>> q;            //pair for storing node* and the horizontal height of that node;
+    q.push({root,0});
+    while(!q.empty()){
+        node *temp = q.front().first;
+        int horizontal_height_of_temp = q.front().second;
+        q.pop(); //pop out from queue;             
+        m[horizontal_height_of_temp] = temp->data;		//we have to keep updating wether height is 0 or not 
+														//Because we want the last element with same horizontal height
+        if(temp -> left){
+            q.push({temp->left, horizontal_height_of_temp-1});
+        }
+        if(temp -> right){
+            q.push({temp->right, horizontal_height_of_temp+1});
+        }
+    }
+    // Itterate over map
+    for(auto x:m){
+        result.push_back(x.second);
+    }
+    return result;
+}
+
+int main()
+{
+    node *root = NULL;
+    root = create_node(4);
+    root->left = create_node(9);
+    root->left->right = create_node(2);
+    root->left->left = create_node(3);
+    root->left->left->right = create_node(1);
+    root->right = create_node(8);
+    root->right->left = create_node(7);
+    root->right->right = create_node(6);
+	root->right->right->left = create_node(45);
+    vector <int> res = buttomView(root);
+    for(auto i=res.begin(); i<res.end(); i++){
+        cout<<*i<<" ";
+    }
+	cout<<"\n";
+	return 0;
+}
+
+//More approach are on geekforgeek
+
