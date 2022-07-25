e2b1042fb93041914e9ee591d197a6aa420676b4
anxmukul <kmukul1197@gmail.com>
2022-07-25 12:10:25 +0530

    Create Graph using vector of array

---
+++ Graph/create.cpp
@@ -0,0 +1,34 @@
+// Creating Graph using vector of array
+#include <bits/stdc++.h>
+using namespace std;
+
+void addEdge(vector <int> adjList[], int u, int v){
+    adjList[u].push_back(v);
+    adjList[v].push_back(u);
+
+}
+
+void printGraph(vector <int> adjList[], int V){
+    for(int i=0; i<V; i++){
+        cout<<"Adjacency List of vertex "<<i<<":\n"<<i;
+        for(auto v:adjList[i]){
+            cout<<"->"<<v;
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    int V;
+    cout<<"Enter no. of Edge:\n";
+    cin>>V;
+    vector <int> adj[V];
+    int u,v;
+    cout<<"Enter nodes/vertices connected with every edge\n";
+    for(int i=0; i<V; i++){
+        cin>>u>>v;
+        addEdge(adj, u, v);
+    }
+    printGraph(adj, V);
+    return 0;
+}
